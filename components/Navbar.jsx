34269fb9bcc3fb68b8dd2698bb57603eec702129
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-20 10:18:03 -0800

    Navbar.jsx content added with motion.nav for Navbar function based on motion.js for navbar to scroll in

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
 'use client';
 
-const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+import { motion } from 'framer-motion';
 
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial='hidden'
+    whileInView='show'
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    navbar
+  </motion.nav>
+);
 export default Navbar;
