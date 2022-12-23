ebeb8067b5b56396a3346a22859a5342dfd70a19
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-23 14:33:21 -0800

    In CustomTexts.jsx, motion.p is added for TypingText fuction

--- sections/About.jsx
+++ sections/About.jsx
@@ -11,7 +11,7 @@ const About = () => (
     <motion.div
       variants={staggerContainer}
       initial='hidden'
-      whitelInView='show'
+      whileInView='show'
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
