205163d97c75431cccb79447a6090bd0f7b8d1f0
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-28 14:15:53 -0800

    In Footer.jsx, below the img inside button, created a self closing div for a line, and below that inside another div, copyright p tag section and social icons added in another div to complete the Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
@@ -27,6 +29,27 @@ const Footer = () => (
             Enter Metaverse
           </span>
         </button>
+      </div>
+      <div className='flex flex-col'>
+        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+
+        <div className='flex items-center justify-between flex-wrap gap-4'>
+          <h4 className='font-extrabold text-[24px] text-white'>METAVERSUS</h4>
+          <p className='font-normal text-[14px] text-white opacity-50'>
+            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+          </p>
+
+          <div className='flex gap-4'>
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className='w-[24px] h-[24px] object-contain cursor-pointer'
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   </motion.footer>
